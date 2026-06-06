3d64ad16cd365ec0c2d4f7ba1e8bd2331ae69ea7
XKKDD <ray981208@apps.ntpc.edu.tw>
2026-06-06 10:04:06 +0000

    123

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    // 抓到選單元素
-    const menu = document.getElementById("hamburger-menu-links");
-    // 切換 "active" 這個 class (有的話就刪掉，沒有就加上去)
-    menu.classList.toggle("active");
-}
